148a5a458ad495f4d0e1e3e6ec006ebda4dfad28
Raghavendar Lokineni <raghavendralokineni@gmail.com>
2018-01-08 13:04:11 +0530

    updated with few enhancements

--- puzzles/encryption.py
+++ puzzles/encryption.py
@@ -3,28 +3,29 @@ solution to the below puzzle:
 https://www.hackerrank.com/challenges/encryption/problem
 '''
 
-from math import ceil, floor, sqrt
+from math import ceil, sqrt
 from string import whitespace
 
 def encrypt(string):
     enc_string = string.translate(None,whitespace)
     length = len(enc_string)
-    rows = int(floor(sqrt(length)))
-    columns = int(ceil(sqrt(length)))
-    split_string = [enc_string[i:i+columns] for i in range(0, len(enc_string), columns)]
-    new_string = []
-    for string in split_string:
 
-        for i in range(len(string)):
-            try:
-                new_string[i] = new_string[i].replace(new_string[i], new_string[i] + string[i])
-            except IndexError:
-                new_string.append(string[i])
+    #calculate the no of colums for the grid
+    columns = int(ceil(sqrt(length)))
+
+    #form a list of strings each of length equal to columns
+    split_string = [enc_string[i:i+columns] for i in range(0, len(enc_string), columns)]
+
+    #encrypted list of strings
+    new_string = ['']*columns
+    for string in split_string:
+        for i, char in enumerate(string):
+            #replace the element in the list with the new string
+            new_string[i] += char
 
     return " ".join(new_string)
 
 if __name__ == "__main__":
     s = raw_input().strip()
-    #s = "if man was meant to stay on the ground god would have given us roots"
     result = encrypt(s)
     print(result)
